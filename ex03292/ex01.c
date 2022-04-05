ab943b99e1458fb740834b7e21a950a36a5045cb
강성훈 <80798929+ksh5324@users.noreply.github.com>
2022-04-05 10:58:50 +0900

    setting

---
+++ ex03292/ex01.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#define MAXLINE 100
+
+// 문자열을 입력 받아 긴 문자열일 때 출력 ...하는 프로그램
+void copy(char from[], char to[]);
+char line[MAXLINE];
+char instr[MAXLINE];
+
+int main(){
+	// 정상종료 일때는 0을 리턴한다.
+	return 0;
+}
+void copy(char from[], char to[]){
+
