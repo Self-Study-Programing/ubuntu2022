515c847b5dd2b911de7fbe1250a4682adda6b4eb
강성훈 <80798929+ksh5324@users.noreply.github.com>
2022-05-18 14:14:53 +0900

    finish

--- fileSystem/dir.c
+++ fileSystem/dir.c
@@ -1,7 +1,22 @@
-void makeDir(){
+#include <stdio.h>
+#include <unistd.h>
+#include <dirent.h>
+#include <sys/stat.h>
 
+void makeDir(){
+    printf("디렉토리 이름을 입력하세요: ");
+    char filename[255] = "";
+
+    scanf("%s", filename);
+
+    mkdir(filename, 0755);
 }
 
 void rmDir(){
+    printf("디렉토리 이름을 입력하세요: ");
+    char filename[255] = "";
 
+    scanf("%s", filename);
+
+    rmdir(filename);
 }
